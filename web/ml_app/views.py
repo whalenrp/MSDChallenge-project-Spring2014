6883607f2765144009de15f950ce31947145fef6
Richard Whalen <richard.whalen1@gmail.com>
2014-04-12 12:20:41 -0500

    Added json and learner persistance

--- web/ml_app/views.py
+++ web/ml_app/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
+import os, sys
+lib_path = os.path.abspath('/home/richard/website/mlproject/MSDChallenge-project-Spring2014/src')
+sys.path.append(lib_path)
+
+from CollaborativeUsersPredictor import CollaborativeUsersPredictor
+
+predictor = CollaborativeUsersPredictor()
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
 
 def user_page(request, user_id):
-    return HttpResponse("Hello, " + user_id + "!")
+
+    predictions = predictor.predict(predictor.user2songs.values()[int(user_id)])
+    return HttpResponse(json.dumps(map(lambda x: int(x), predictions)))
