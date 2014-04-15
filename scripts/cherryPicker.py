229d1ee1c8594424db0ad7ff2d319aabe20f6b79
Nimous <jenna.a.careccia@vanderbilt.edu>
2014-04-14 20:00:32 -0500

    backdrop for getting all fields built

--- scripts/cherryPicker.py
+++ scripts/cherryPicker.py
@@ -3,34 +3,59 @@
 # from msd h5 files
 
 # referencing: http://stackoverflow.com/questions/2212643/python-recursive-folder-read
+# and		 : http://stackoverflow.com/questions/8024248/telling-python-to-save-a-txt-file-to-a-certain-directory-on-windows-and-mac
+
 import os
 import sys
-#import hdf5_functions
+import hdf5_functions
 
-#rootdir = sys.argv[1]
+
+savePath = "C:\\Users\\Aluminum\\Desktop\\"
+outfilePath = os.path.join(savePath,"test.txt")
+songIdDict = {"init":"init"}
+keyDict = {"init":"init"}
 
 # process taste_profile_song_to_tracks to map filename -> songID
+# replace with true filename when ready
+with open ("taste_profile_song_to_tracks.txt",'r') as r:
+	for line in r:
+		splitLine = line.split(' ')
+		for fileName in splitLine: # handles multiple filenames -> 1 songID
+			if splitLine.index(fileName) != 0:
+				songIdDict["" + fileName.rstrip()] = "" + splitLine[0].rstrip()
 
-# process kaggle_songs 				   to map songID -> key
+# process kaggle_songs to map songID -> key
+with open("kaggle_songs.txt",'r') as r:
+	for line in r:
+		songID, key = line.split(' ')[:2]
+		keyDict["" + songID.rstrip()] = "" + key.rstrip()
 
-# 1) determine current file name
-# 2) look up songID in taste_profile_song_to_tracks
-# 3) look up key using songID in kaggle_songs
+
 # 4) pull artist_name from h5
 # 5) pull song title from h5
 
 # store data in a written format
 # txt file --> fname songID key artist_name song_title
 
-#for root, subFolders, files in os.walk(rootdir):
-outfileName = "C:\Users\Aluminum\Desktop\test.txt"
-with open("test.txt", 'w') as fileOut:
-	#for fileName in files:
-	#	filePath = os.path.join(root,fileName)
-		
-	#	with open(filePath, 'r') as f:
-	#		toWrite = f.read()
-	#		fileOut.write("The file %s contains %s" % (filePath,toWrite))
-	#		fileOut.write(toWrite)
-	fileOut.write("Hi!\n")
-	fileOut.write("Hi again!")
+# change rootdir according to where the script is
+rootdir = "C:\\Users\\Aluminum\\Documents\\Academics\\2014Spring\\CS292\\FinalProject\\MSDChallenge-project-Spring2014\\scripts\\HoldsA\\"
+for root, subFolders, files in os.walk(rootdir):
+	with open(outfilePath, 'a') as fileOut:
+			
+		for fileName in files:
+		#	filePath = os.path.join(root,fileName)
+			
+		#	with open(filePath, 'r') as f:
+		#		toWrite = f.read()
+		#		fileOut.write("The file %s contains %s" % (filePath,toWrite))
+		#		fileOut.write(toWrite)
+			trackFileName = os.path.splitext(fileName)[0]
+			songID = songIdDict[trackFileName]
+			key = keyDict[songID]
+			
+			artist_name = hdf5_functions.get_artist_name(h5)
+			title = hdf5_functions.get_title(h5)
+			
+			
+			fileOut.write(key + " " + trackFileName + " " + songID + "\n")
+			
