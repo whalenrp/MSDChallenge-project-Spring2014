4c6f090aefe3325e78dc486ff517d6c77882d3de
Chantelle Cheng <chantelle.s.cheng@vanderbilt.edu>
2014-04-16 11:15:45 -0500

    fixed bugs in code, now works

--- scripts/cherryPicker.py
+++ scripts/cherryPicker.py
@@ -14,42 +14,41 @@ import sys
 import hdf5_functions
 
 
-savePath = "C:\\Users\\Aluminum\\Desktop\\"
+savePath = "C:\\Users\\meow\\Downloads\\"
 outfilePath = os.path.join(savePath,"test.txt")
 songIdDict = {"init":"init"}
 keyDict = {"init":"init"}
 
 # process taste_profile_song_to_tracks to map filename -> songID
-with open ("taste_profile_song_to_tracks.txt",'r') as r:
+with open ("E:\\Kaggle\\taste_profile_song_to_tracks.txt",'r') as r:
 	for line in r:
-		splitLine = line.split(' ')
-		for fileName in splitLine: # handles multiple filenames -> 1 songID
-			if splitLine.index(fileName) != 0:
-				songIdDict["" + fileName.rstrip()] = "" + splitLine[0].rstrip()
+		splitLine = line.split('\t')
+		for fileName in splitLine[1:]: # handles multiple filenames -> 1 songID
+			songIdDict[str(fileName.rstrip())] = str(splitLine[0].rstrip())
 
 # process kaggle_songs to map songID -> key
-with open("kaggle_songs.txt",'r') as r:
+with open("E:\\Kaggle\\kaggle_songs.txt",'r') as r:
 	for line in r:
 		songID, key = line.split(' ')[:2]
 		keyDict["" + songID.rstrip()] = "" + key.rstrip()
 
 # change rootdir according to where the source h5 files are
-rootdir = "C:\\Users\\Aluminum\\Documents\\Academics\\2014Spring\\CS292\\FinalProject\\MSDChallenge-project-Spring2014\\scripts\\subfolder\\"
+rootdir = "E:\\A"
+fileOut = open(outfilePath, 'w')
 for root, subFolders, files in os.walk(rootdir):
-	with open(outfilePath, 'a') as fileOut:
 			
 		for fileName in files:
 			if fileName.lower().endswith(".h5"):
 				trackFileName = os.path.splitext(fileName)[0]
-				songID = songIdDict[trackFileName]
-				key = keyDict[songID]
-				
-				# I am continuing under the assumption this is correct
-				h5 = hdf5_functions.open_h5_file_read(fileName)
-				artistName = hdf5_functions.get_artist_name(h5)
-				title = hdf5_functions.get_title(h5)
-				
-				
-				fileOut.write(key + " " + trackFileName + " " 
-						      + songID + " " + artistName + " " + title + "\n")
-			
+				if trackFileName in songIdDict:
+					songID = songIdDict[trackFileName]
+					key = keyDict[songID]
+
+					# I am continuing under the assumption this is correct
+					h5 = hdf5_functions.open_h5_file_read(os.path.join(root, fileName))
+					artistName = hdf5_functions.get_artist_name(h5)
+					title = hdf5_functions.get_title(h5)
+					h5.close()
+					
+					fileOut.write(key + "\t" + trackFileName + "\t" + songID + "\t" + artistName + "\t" + title + "\n")
+fileOut.close()
